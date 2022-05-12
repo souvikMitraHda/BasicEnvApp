55be0cec9a96848c3aca09fd36a9d56d414378b5
souvikMitraHda <42381596+souvikMitraHda@users.noreply.github.com>
2022-05-12 14:22:27 +0200

    Add files via upload

---
+++ src/main/java/com/test/envVariableCheck/EnvVariableCheckApplication.java
@@ -0,0 +1,13 @@
+package com.test.envVariableCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvVariableCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvVariableCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/envVariableCheck/EnvVariableCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.envVariableCheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvVariableCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
